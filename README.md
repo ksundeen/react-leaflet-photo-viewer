a22a52a1948094af26d7bd65751e5e195d1789dc
Kim Sundeen <kimberly.sundeen@heathus.com>
2022-05-10 06:12:49 -0500

    Converted to progressive web app
    Added progressive web app service worker to app to run offline.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-leaflet-photo-viewer
 Simple vacation photo viewer using react leaflet. Photos, label markers, and custom marker locations are stored in a json file with relevant information.
+
+To run local build:
+1. Install nodejs and npm. Preferably, use node version manager (nvm)
+2. Run > npm install
+3. Run > npm start
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // Copied from https://stackoverflow.com/questions/46793310/fetch-local-json-file-from-public-folder-reactjs
 fetch('./data/familytrips.json')
@@ -23,7 +24,10 @@ fetch('./data/familytrips.json')
   );
 })
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
